27c1497d8d9eda7478b15c147db232715991aacd
lcatmiao <lcatmiao@outlook.com>
2022-02-04 15:21:30 +0800

    Personalize some configs

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220204.1206';
+const cacheName = 'chirpy-20220204.1511';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
