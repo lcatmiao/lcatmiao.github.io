f505806ddb8caaed8bdec79bc9b6950b778556a9
lcatmiao <lcatmiao@outlook.com>
2022-02-05 15:17:39 +0800

    Add 'Jekyll Chirpy theme + Github搭建个人博客.md'

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220205.1416';
+const cacheName = 'chirpy-20220205.1516';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
