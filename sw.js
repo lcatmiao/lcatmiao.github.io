10bff07481873443087876cfadc669719ec16136
lcatmiao <lcatmiao@outlook.com>
2022-02-05 14:17:26 +0800

    Add favicons with permissions changed

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220205.1250';
+const cacheName = 'chirpy-20220205.1416';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
