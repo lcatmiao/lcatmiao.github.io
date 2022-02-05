ba1187dab1bfb17fd225d97d1cae1c182f56d0ac
lcatmiao <lcatmiao@outlook.com>
2022-02-05 13:11:19 +0800

    Modify git如何合并分支.md
    Add a concise description in the head of the file.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220205.1102';
+const cacheName = 'chirpy-20220205.1250';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
