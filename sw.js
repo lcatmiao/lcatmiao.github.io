d6252e4cc2ed5d3d4dfe4a565bb5e8792a505846
lcatmiao <lcatmiao@outlook.com>
2022-12-18 19:02:05 +0800

    Mod 手动搭建Web服务器环境.md

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221207.2216';
+const cacheName = 'chirpy-20221218.1859';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
