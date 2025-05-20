5af1badefe835ff1dfefd643d7cf36dbed63df98
lcatmiao <lcatmiao@outlook.com>
2025-05-20 22:42:50 +0800

    20250520224237

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250424.2248';
+const cacheName = 'chirpy-20250520.2242';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
