dd04e4dce6b63c87f33f3b3f631bb20d7c6b8864
lcatmiao <lcatmiao@outlook.com>
2026-08-26 20:13:19 +0800

    20260826201302

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1787737435',resources: [
+    cacheName: 'chirpy-1787746259',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
